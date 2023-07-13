985fb82114240c8e03f0555b1267d8353cf8f67f
Dang-Tran <trmdang@my.yorku.ca>
2023-07-13 09:00:04 -0400

    Add documentation.txt

--- __main__.py
+++ __main__.py
@@ -16,42 +16,54 @@ def get_python_files(project):
                 py_files.append(os.path.join(root, file))
     return py_files
 
+
 def call_parser(py_file):
     return parser.to_ast(py_file)
 
+
 def call_identifier(name, project, codes):
     return identifier.identify(name, project, codes)
+
 
 def call_classifier(identifier_result_file):
     print("+++ Starting classification...")
     classifier.classify(identifier_result_file)
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description="Retrieve which code imports which file in the project")
+    parser = argparse.ArgumentParser(
+        description="Retrieve which code imports which file in the project"
+    )
 
     parser.add_argument(
-        '-p',
-        '--project',
+        "-p",
+        "--project",
         default=None,
         type=str,
-        dest='project',
-        help="Relative or absolute path of the project to analyse")
-    
+        dest="project",
+        help="Relative or absolute path of the project to analyse",
+    )
+
     parser.add_argument(
-        '-n',
-        '--name',
-        default='',
+        "-n",
+        "--name",
+        default="",
         type=str,
-        dest='name',
-        help="Set the name of the project. Defaults to the name of the directory to analyse")
-    
+        dest="name",
+        help="Set the name of the project. Defaults to the name of the directory to analyse",
+    )
+
     return parser
+
 
 def main():
     args = get_args().parse_args()
     assert args.project is not None, "The path of the ML project must be specified"
-    name = args.name if args.name != '' else os.path.basename(os.path.normpath(args.project))
-
+    name = (
+        args.name
+        if args.name != ""
+        else os.path.basename(os.path.normpath(args.project))
+    )
     filenames = []
     codes = {}
     for root, _, files in os.walk(args.project):
@@ -66,7 +78,6 @@ def main():
     identifier_result_file = call_identifier(name, os.path.abspath(args.project), codes)
     if identifier_result_file:
         call_classifier(identifier_result_file)
-
 
 
 if __name__ == "__main__":
