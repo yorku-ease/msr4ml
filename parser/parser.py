948952a2405ac886efbad32d5499c95b62f58a2f
Dang-Tran <trmdang@my.yorku.ca>
2023-07-16 20:08:57 -0400

    Restore default

--- parser/parser.py
+++ parser/parser.py
@@ -1,35 +1,10 @@
-# from astroid import parse
-# import astroid
-# from test import infer_join, _looks_like_infer_join
+from astroid import parse
 
-# module = astroid.parse('''
-# "my name is {}".format(name)
-# ''')
-
-# print(module);
-
-# def to_ast(py_file):
-#     return parse(py_file)
-
-# def main():
-#     pass
-
-# if __name__ == "__main__":
-#     main()
-import astroid
-from astroid import MANAGER, nodes
-import test
-
+def to_ast(py_file):
+    return parse(py_file)
 
 def main():
-    code = """
-    path = join('dir', 'file.txt')
-    """
-    tree = astroid.parse(code)
-
-    print("Original Code:")
-    print(tree.repr_tree())
-
+    pass
 
 if __name__ == "__main__":
     main()
